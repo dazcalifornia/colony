450c8a54f9227ec5cf40b201745f7264e048b497
Supakit Wiwustrinon <franx@FRANX-2.local>
2023-08-05 05:11:05 +0700

    add multiLanguage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,34 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// _app.js
+import { useEffect } from "react";
+import { useTranslation, initReactI18next } from "react-i18next";
+import i18n from "i18next";
+import enTranslation from "../../locales/en.json";
+import thTranslation from "../../locales/th.json";
+import jpTranslation from "../../locales/jp.json";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const resources = {
+  en: { translation: enTranslation },
+  th: { translation: thTranslation },
+  jp: { translation: jpTranslation },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en", // Default language
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+function MyApp({ Component, pageProps }: any) {
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    // Preload other languages
+    i18n.loadLanguages("th");
+  }, []);
+
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
